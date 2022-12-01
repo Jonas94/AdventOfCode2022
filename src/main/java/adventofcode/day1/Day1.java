2cd5728aab6c13f296dde33b13a81e2591b1bbc8
Jonas Fredriksson <jonas.fredriksson94@gmail.com>
2022-12-01 14:48:20 +0100

    Refactor day 1

--- src/main/java/adventofcode/day1/Day1.java
+++ src/main/java/adventofcode/day1/Day1.java
@@ -1,62 +1,51 @@
 package adventofcode.day1;
 
 import adventofcode.utils.FileHandler;
+import adventofcode.utils.ParseHelper;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.*;
 
 public class Day1 {
-    public int solveDay1(String inputFile) {
-        Map<Integer, List<Integer>> caloriesMap = readStringsToNumbers(inputFile);
+
+    public int part1(String inputFile) {
+        Map<Integer, List<Integer>> caloriesMap = readStringsToMapOfCalorieLists(inputFile);
 
         return sumEveryBackpack(caloriesMap).stream().mapToInt(v -> v).max().orElseThrow();
     }
 
-    public int solveDay1Part2(String inputFile) {
-        Map<Integer, List<Integer>> caloriesMap = readStringsToNumbers(inputFile);
-        List<Integer> summedCalories = sumEveryBackpack(caloriesMap);
-        Collections.sort(summedCalories);
-        Collections.reverse(summedCalories);
+    public int part2(String inputFile) {
+        Map<Integer, List<Integer>> caloriesMap = readStringsToMapOfCalorieLists(inputFile);
 
-        int sumOfTop3HighestCalories = 0;
-        for (int i = 0; i < 3; i++) {
-            sumOfTop3HighestCalories += summedCalories.get(i);
-        }
-
-        return sumOfTop3HighestCalories;
+        return sumEveryBackpack(caloriesMap)
+                .stream()
+                .sorted(Collections.reverseOrder())
+                .limit(3)
+                .reduce(0, Integer::sum);
     }
 
     private List<Integer> sumEveryBackpack(Map<Integer, List<Integer>> calorieMap) {
         List<Integer> calorieList = new ArrayList<>();
-        for (Map.Entry<Integer, List<Integer>> entry : calorieMap.entrySet()) {
-            int elfCalorieSum = 0;
-            for (Integer calorie : entry.getValue()) {
-                elfCalorieSum += calorie;
-            }
-            calorieList.add(elfCalorieSum);
-        }
+        calorieMap.forEach((key, value) -> calorieList.add(value.stream().reduce(0, Integer::sum)));
 
         return calorieList;
     }
 
-    private Map<Integer, List<Integer>> readStringsToNumbers(String inputFile) {
-        List<String> numberStrings = FileHandler.readFileIntoList(inputFile);
-        numberStrings.add(""); //Empty string to add the last elf
+    private Map<Integer, List<Integer>> readStringsToMapOfCalorieLists(String inputFile) {
+        List<String> inputStrings = FileHandler.readFileIntoList(inputFile);
+        inputStrings.add(""); //Lazy workaround: empty string to add the last elf
 
         Map<Integer, List<Integer>> caloriesMap = new HashMap<>();
-        List<Integer> numbers = new ArrayList<>();
+        List<Integer> currentElfCalories = new ArrayList<>();
+        int elfId = 0;
 
-        int elfNumber = 0;
-        for (String number : numberStrings) {
-            if (number != null && !number.isEmpty()) {
-                try {
-                    numbers.add(Integer.parseInt(number));
-                } catch (NumberFormatException e) {
-                    System.out.println("Something went wrong parsing number");
-                }
+        for (String calorieAmount : inputStrings) {
+            if (!StringUtils.isEmpty(calorieAmount)) {
+                currentElfCalories.add(ParseHelper.parseIntOrZero(calorieAmount));
             } else {
-                caloriesMap.put(elfNumber, List.copyOf(numbers));
-                numbers.clear();
-                elfNumber++;
+                caloriesMap.put(elfId, List.copyOf(currentElfCalories));
+                currentElfCalories.clear();
+                elfId++;
             }
         }
         return caloriesMap;
--- src/test/java/adventofcode/TestDay1.java
+++ src/test/java/adventofcode/TestDay1.java
@@ -2,31 +2,35 @@ package adventofcode;
 
 import adventofcode.day1.Day1;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class TestDay1 {
+class TestDay1 {
 
-	@Test
-	public void testSolveDay1Part1() {
-		Day1 day1 = new Day1();
-		int result = day1.solveDay1("testinput1.txt");
+    @Test
+    void testPart1() {
+        Day1 day1 = new Day1();
+        int result = day1.part1("testinput1.txt");
 
-		assertEquals(24000, result);
+        assertEquals(24000, result);
 
-		System.out.println(day1.solveDay1("input1.txt"));
-	}
+        System.out.println(day1.part1("input1.txt"));
 
-	@Test
-	public void testSolveDay1Part2() {
-		Day1 day1 = new Day1();
-		int result = day1.solveDay1Part2("testinput1.txt");
+        result = day1.part1("input1.txt");
+        assertEquals(66186, result);
 
-		assertEquals(45000, result);
+    }
 
-		result = day1.solveDay1Part2("input1.txt");
-		System.out.println(day1.solveDay1Part2("input1.txt"));
+    @Test
+    void testPart2() {
+        Day1 day1 = new Day1();
+        int result = day1.part2("testinput1.txt");
 
-		assertEquals(196804, result);
+        assertEquals(45000, result);
 
-	}
+        result = day1.part2("input1.txt");
+        System.out.println(day1.part2("input1.txt"));
+
+        assertEquals(196804, result);
+    }
 }

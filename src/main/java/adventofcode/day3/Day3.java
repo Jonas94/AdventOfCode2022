2c84d51a499a87514c0ee82a3f5fe89fff1c7eb8
Jonas Fredriksson <jonas.fredriksson94@gmail.com>
2022-12-03 15:06:53 +0100

    Refactor day 3

--- src/main/java/adventofcode/day3/Day3.java
+++ src/main/java/adventofcode/day3/Day3.java
@@ -1,26 +1,31 @@
 package adventofcode.day3;
 
+import adventofcode.utils.Converters;
 import adventofcode.utils.FileHandler;
 
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class Day3 {
+    /**
+     * In ASCII table lowercase letters start at 97, uppercase letters start at 65.
+     * We need to transform them to this format:
+     * - Lowercase item types a through z have priorities 1 through 26.
+     * - Uppercase item types A through Z have priorities 27 through 52.
+     */
     private static final int LOWERCASE_REDUCE_AMOUNT = 96;
     private static final int UPPERCASE_REDUCE_AMOUNT = 38;
 
     public long part1(String inputFile) {
         List<String> lines = FileHandler.readFileIntoList(inputFile);
-
         List<Character> duplicatedItems = new ArrayList<>();
+
         for (String line : lines) {
 
             int midChar = line.length() / 2;
-            String firstCompartmentString = line.substring(0, midChar);
-            String secondCompartmentString = line.substring(midChar);
 
-            Set<Character> firstCompartment = firstCompartmentString.chars().mapToObj(e -> (char) e).collect(Collectors.toSet());
-            Set<Character> secondCompartment = secondCompartmentString.chars().mapToObj(e -> (char) e).collect(Collectors.toSet());
+            Set<Character> firstCompartment = Converters.stringToCharSet(line.substring(0, midChar));
+            Set<Character> secondCompartment = Converters.stringToCharSet(line.substring(midChar));
 
             char duplicate = firstCompartment.stream().filter(secondCompartment::contains).findFirst().orElseThrow();
 
@@ -29,13 +34,8 @@ public class Day3 {
 
         int totalPriority = 0;
         for (Character item : duplicatedItems) {
-            int itemNumber = item;
-            if (itemNumber >= 96) {
-                itemNumber -= LOWERCASE_REDUCE_AMOUNT;
-            } else {
-                itemNumber -= UPPERCASE_REDUCE_AMOUNT;
-            }
-            totalPriority += itemNumber;
+            int asciiCode = item;
+            totalPriority += convertFromAsciiToPriority(asciiCode);
         }
 
         return totalPriority;
@@ -44,36 +44,41 @@ public class Day3 {
     public long part2(String inputFile) {
         List<String> lines = FileHandler.readFileIntoList(inputFile);
 
-
         int totalPriority = 0;
         for (int i = 0; i < lines.size(); i += 3) {
 
-            Set<Character> firstRucksack = lines.get(i).chars().mapToObj(e -> (char) e).collect(Collectors.toSet());
-            Set<Character> secondRucksack = lines.get(i + 1).chars().mapToObj(e -> (char) e).collect(Collectors.toSet());
-            Set<Character> thirdRucksack = lines.get(i + 2).chars().mapToObj(e -> (char) e).collect(Collectors.toSet());
-            int itemNumber = findCommonChar(List.of(firstRucksack, secondRucksack, thirdRucksack));
-            if (itemNumber >= 96) {
-                itemNumber -= LOWERCASE_REDUCE_AMOUNT;
-            } else {
-                itemNumber -= UPPERCASE_REDUCE_AMOUNT;
-            }
-            totalPriority += itemNumber;
+            Set<Character> firstRucksack = Converters.stringToCharSet(lines.get(i));
+            Set<Character> secondRucksack = Converters.stringToCharSet(lines.get(i + 1));
+            Set<Character> thirdRucksack = Converters.stringToCharSet(lines.get(i + 2));
+            int asciiCode = findCommonChar(List.of(firstRucksack, secondRucksack, thirdRucksack));
+            totalPriority += convertFromAsciiToPriority(asciiCode);
         }
-
 
         return totalPriority;
     }
 
-    private char findCommonChar(List<Set<Character>> rucksacks) {
 
+    private int convertFromAsciiToPriority(int asciiCode) {
+        int priority;
+        if (asciiCode >= LOWERCASE_REDUCE_AMOUNT) {
+            priority = asciiCode - LOWERCASE_REDUCE_AMOUNT;
+        } else {
+            priority = asciiCode - UPPERCASE_REDUCE_AMOUNT;
+        }
+        return priority;
+    }
+
+    private char findCommonChar(List<Set<Character>> rucksacks) {
         Map<Character, Integer> items = new HashMap<>();
 
         for (Set<Character> rucksack : rucksacks) {
             rucksack.forEach(i -> items.merge(i, 1, Integer::sum));
         }
 
-        return items.entrySet().stream().filter(entry -> entry.getValue() > 2).findFirst().map(Map.Entry::getKey).orElseThrow();
-
+        return items.entrySet().stream()
+                .filter(entry -> entry.getValue() > 2)
+                .findFirst()
+                .map(Map.Entry::getKey).orElseThrow();
     }
 
 }

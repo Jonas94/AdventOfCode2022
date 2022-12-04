9a17b88135889f37219c24abbb8e23784b32530d
Jonas Fredriksson <jonas.fredriksson94@gmail.com>
2022-12-04 11:23:00 +0100

    Day 4 before refactor

---
+++ src/test/java/adventofcode/TestDay4.java
@@ -0,0 +1,28 @@
+package adventofcode;
+
+import adventofcode.day4.Day4;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TestDay4 {
+
+    @Test
+    void part1() {
+        Day4 day = new Day4();
+        long result = day.part1("testinput4.txt");
+        assertEquals(2, result);
+        result = day.part1("input4.txt");
+        assertEquals(464, result);
+    }
+
+    @Test
+    void part2() {
+        Day4 day = new Day4();
+        long result = day.part2("testinput4.txt");
+        assertEquals(4, result);
+        System.out.println(day.part2("input4.txt"));
+        result = day.part2("input4.txt");
+        assertEquals(770, result);
+    }
+}

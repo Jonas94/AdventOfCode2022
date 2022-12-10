2c4b3bc70aa2907467ee13a877cee8873af82947
Jonas Fredriksson <jonas.fredriksson94@gmail.com>
2022-12-10 16:37:38 +0100

    Refactor day 5

--- src/main/java/adventofcode/day5/Day5.java
+++ src/main/java/adventofcode/day5/Day5.java
@@ -12,32 +12,13 @@ public class Day5 {
     public String part1(String inputFile) {
         List<String> lines = FileHandler.readFileIntoList(inputFile);
 
-        List<Stack<String>> stacks = new ArrayList<>();
+        int numberLineIndex = findNumberLine(lines);
 
-        int numberOfStacks = (lines.get(0).length() + 1) / 4;
+        List<Stack<String>> stacks = initiateStacks(lines, numberLineIndex);
 
-        for (int i = 0; i < numberOfStacks; i++) {
-            stacks.add(new Stack<>());
-        }
+        for (int i = numberLineIndex + 2; i < lines.size(); i++) {
 
-        int numberLineId = findNumberLine(lines);
-
-        for (int i = numberLineId - 1; i >= 0; i--) {
-            String[] crates = lines.get(i).split("(?<=\\G.{4})");
-
-            for (int j = 0; j < numberOfStacks; j++) {
-                if (!crates[j].trim().isEmpty()) {
-                    stacks.get(j).add(crates[j]);
-                }
-            }
-        }
-
-        for (int i = numberLineId + 2; i < lines.size(); i++) {
-
-            String line = lines.get(i);
-            line = line.replace("move ", "").replace(" from ", ",").replace(" to ", ",");
-
-            int[] instructions = Arrays.stream(line.split(",")).mapToInt(Integer::parseInt).toArray();
+            int[] instructions = parseLineToInstructions(lines.get(i));
 
             for (int k = 0; k < instructions[0]; k++) {
                 stacks.get(instructions[2] - 1).add(stacks.get(instructions[1] - 1).pop());
@@ -45,46 +26,24 @@ public class Day5 {
         }
 
         StringBuilder stringBuilder = new StringBuilder();
-        for (Stack stack : stacks) {
+        for (Stack<String> stack : stacks) {
             stringBuilder.append(stack.pop());
         }
 
-        return stringBuilder.toString().replace("[", "").replace("]", "").replace(" ", "");
+        return getFormattedResult(stringBuilder.toString());
     }
 
     public String part2(String inputFile) {
         List<String> lines = FileHandler.readFileIntoList(inputFile);
+        int numberLineIndex = findNumberLine(lines);
+        List<Stack<String>> stacks = initiateStacks(lines, numberLineIndex);
 
-        List<Stack<String>> stacks = new ArrayList<>();
+        for (int i = numberLineIndex + 2; i < lines.size(); i++) {
 
-        int numberOfStacks = (lines.get(0).length() + 1) / 4;
-
-        for (int i = 0; i < numberOfStacks; i++) {
-            stacks.add(new Stack<>());
-        }
-
-        int numberLineId = findNumberLine(lines);
-
-        for (int i = numberLineId - 1; i >= 0; i--) {
-            String[] crates = lines.get(i).split("(?<=\\G.{4})");
-
-            for (int j = 0; j < numberOfStacks; j++) {
-                if (!crates[j].trim().isEmpty()) {
-                    stacks.get(j).add(crates[j]);
-                }
-            }
-        }
-
-        for (int i = numberLineId + 2; i < lines.size(); i++) {
-
-            String line = lines.get(i);
-            line = line.replace("move ", "").replace(" from ", ",").replace(" to ", ",");
-
-            int[] instructions = Arrays.stream(line.split(",")).mapToInt(Integer::parseInt).toArray();
+            int[] instructions = parseLineToInstructions(lines.get(i));
 
             Stack<String> tempStack = new Stack<>();
             Stack<String> newStack = stacks.get(instructions[2] - 1);
-
             Stack<String> oldStack = stacks.get(instructions[1] - 1);
 
             for (int k = 0; k < instructions[0]; k++) {
@@ -95,19 +54,51 @@ public class Day5 {
                 newStack.add(tempStack.pop());
             }
         }
-        
+
 
         StringBuilder stringBuilder = new StringBuilder();
-        for (Stack stack : stacks) {
+        for (Stack<String> stack : stacks) {
             stringBuilder.append(stack.pop());
         }
 
-        return stringBuilder.toString().replace("[", "").replace("]", "").replace(" ", "");
+        return getFormattedResult(stringBuilder.toString());
+
+    }
+
+    private String getFormattedResult(String string) {
+        return string.replace("[", "").replace("]", "").replace(" ", "");
+    }
+
+    private int[] parseLineToInstructions(String line) {
+        line = line.replace("move ", "").replace(" from ", ",").replace(" to ", ",");
+
+        return Arrays.stream(line.split(",")).mapToInt(Integer::parseInt).toArray();
+
+    }
+
+    private List<Stack<String>> initiateStacks(List<String> lines, int numberLineIndex) {
+        List<Stack<String>> stacks = new ArrayList<>();
+
+        int numberOfStacks = (lines.get(0).length() + 1) / 4;
+
+        for (int i = 0; i < numberOfStacks; i++) {
+            stacks.add(new Stack<>());
+        }
+        
+        for (int i = numberLineIndex - 1; i >= 0; i--) {
+            String[] crates = lines.get(i).split("(?<=\\G.{4})");
+
+            for (int j = 0; j < numberOfStacks; j++) {
+                if (!crates[j].trim().isEmpty()) {
+                    stacks.get(j).add(crates[j]);
+                }
+            }
+        }
+        return stacks;
 
     }
 
     private int findNumberLine(List<String> lines) {
-
         for (int i = 0; i < lines.size(); i++) {
             if (lines.get(i).startsWith(" 1")) {
                 return i;
